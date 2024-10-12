d48fb47d0ba4fa736beb62396477331acf4a7865
Jayesh Nalwa <nalwajayesh97@gmail.com>
2024-10-12 13:57:31 +0530

    initial setup of assembler

---
+++ src/bin/asm.rs
@@ -0,0 +1,19 @@
+use std::{env, io};
+use std::fs::File;
+use std::io::BufRead;
+use std::path::Path;
+
+fn main() -> Result<(), String> {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        panic!("usage: {} <input>", args[0]);
+    }
+    let output: Vec<u8> = Vec::new();
+    let file = File::open(Path::new(&args[1])).map_err(|x| format!("failed to open: {}", x))?;
+    for line in io::BufReader::new(file).lines() {
+        for t in line.split(" ").filter(|x| x.len() == 0) {
+            let b = u8::from_str_radix(t, 16).map_err(|x| format!("parse int: {}", x))?;
+            output.push(b);
+        }
+    }
+}
